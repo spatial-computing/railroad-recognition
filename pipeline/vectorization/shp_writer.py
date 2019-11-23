cd8a0944d79c6f7d758c754997739bc06ff7477a
Weiwei Duan <weiweidu@usc.edu>
2019-11-22 18:49:23 -0800

    Update shp_writer.py
    add a function to convert coordinates from tif to any espg

--- pipeline/vectorization/shp_writer.py
+++ pipeline/vectorization/shp_writer.py
@@ -9,19 +9,39 @@ def pixel2coord(coor_path, x, y):
     yp = d * x + e * y + yoff
     return(xp, yp)
 
-def createShapefile(shapefileName, line, coor_path):
+def coor2epsg(coor_path, epsg, x, y):
+    # get CRS from dataset
+    ds = gdal.Open(coor_path)
+    crs = osr.SpatialReference()
+    # crs.ImportFromProj4(ds.GetProjection())
+    crs.ImportFromWkt(ds.GetProjectionRef())
+# create lat/long crs with WGS84 datum
+    crsGeo = osr.SpatialReference()
+    crsGeo.ImportFromEPSG(epsg)
+    t = osr.CoordinateTransformation(crs, crsGeo)
+    (lat, long, z) = t.TransformPoint(x, y)
+    return (lat, long)
+
+def createShapefile(shapefileName, line, coor_path, epsg=None):
   # Getting shapefile driver
     driver = ogr.GetDriverByName('ESRI Shapefile')
     # Creating a new data source and layer
     if os.path.exists(shapefileName):
         driver.DeleteDataSource(shapefileName)
+    if epsg == None:
+        srs = gdal.Open(coor_path).GetProjection()
+        spatial_reference = osr.SpatialReference()
+        spatial_reference.ImportFromProj4(srs)
+    else:
+        spatial_reference = osr.SpatialReference()
+        spatial_reference.ImportFromEPSG(epsg)
 
     ds = driver.CreateDataSource(shapefileName)
     if ds is None:
         print 'Could not create file'
         sys.exit(1)
 
-    layer = ds.CreateLayer('layerName', geom_type = ogr.wkbLineString)
+    layer = ds.CreateLayer('layerName', spatial_reference, geom_type = ogr.wkbLineString)
     # add a field to the output
     fieldDefn = ogr.FieldDefn('fieldName', ogr.OFTReal)
     layer.CreateField(fieldDefn)
@@ -33,6 +53,8 @@ def createShapefile(shapefileName, line, coor_path):
             p = m.split(' ')
             x, y = float(p[0]), float(p[1])
             xp, yp = pixel2coord(coor_path,x,y)
+            if epsg != None:
+                xp, yp = coor2epsg(coor_path, epsg, xp, yp)
             lineString.AddPoint(xp,yp)
 
         featureDefn = layer.GetLayerDefn()
